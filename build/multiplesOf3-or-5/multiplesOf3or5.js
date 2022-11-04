ed0e3d042ce2303a7b6c3832fa2f252c60e8e45d
brennan fox <klbrennanfox@protonmail.com>
2022-11-03 22:37:24 -0400

    Project setup and first problem

---
+++ build/multiplesOf3-or-5/multiplesOf3or5.js
@@ -0,0 +1,14 @@
+"use strict";
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+// The sum of these multiples is 23.
+// PROBLEM - Find the sum of all the multiples of 3 or 5 below 1000.
+let sum = 0;
+for (let index = 0; index < 1000; index++) {
+    const divBy5 = index % 5;
+    const divBy3 = index % 3;
+    if (divBy5 === 0 || divBy3 === 0) {
+        console.log("divBy3", index, divBy3);
+        sum = sum + index;
+    }
+}
+console.log(sum);
